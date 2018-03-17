21b534a415c638f195e0954dd403cc9d7c0af140
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2018-03-17 14:11:27 +0530

    Create foreign key in restaurant and menu to link their creator id.

--- database_setup.py
+++ database_setup.py
@@ -10,9 +10,9 @@ class User(base):
     __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
-    picture = Column(String(250), nullable=False)
+    name = Column(String(250))
+    picture = Column(String(250))
 
 
 class Restaurant(Base):
@@ -20,6 +20,8 @@ class Restaurant(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
@@ -39,7 +41,9 @@ class MenuItem(Base):
     price = Column(String(8))
     course = Column(String(250))
     restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
     restaurant = relationship(Restaurant)
+    user = relationship(User)
 
     @property
     def serialize(self):
